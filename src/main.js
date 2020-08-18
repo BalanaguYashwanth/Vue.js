256975f95b896a3dac293869e3b4bcdc15180e7b
BalanaguYashwanth <balanaguyashwanth@gmail.com>
2020-08-18 11:25:37 +0530

    main js changes

--- src/main.js
+++ src/main.js
@@ -1,12 +1,33 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource'
-
 Vue.use(VueResource)
-
 Vue.config.productionTip = false
 
+
 export const bus = new Vue()
+
+
+
+Vue.directive('size', {
+  bind(el, binding){
+      if (binding.value == 'wide'){
+          el.style.maxWidth = "1260px";
+      } 
+      if(binding.arg == 'column'){
+          el.style.background = '#ddd';
+          el.style.padding = '20px';
+      }
+  }
+});
+
+
+
+
+Vue.filter('short',function(value){
+  return value.slice(0,100)+".."
+})
+
 
 new Vue({
   render: h => h(App),
